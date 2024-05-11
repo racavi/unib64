7b628038e5c5ea58bc2e01dfece717cbb21d07bb
Rafa Calvo <169667901+rafacalvo@users.noreply.github.com>
2024-05-11 20:19:12 +0200

    feat - add original implementation from mdn page

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # unib64
+
+This NodeJS package Converts Unicode text to Base64 string and viceversa.
+
+## Motivation
+
+This package is entirely based The "Unicode Problem" solution as depicted into Mozilla Developers web (See [References](#references)).
+
+## Use Cases
+
+- Encode Environment variables expected to hold arbitrary Unicode text.
+
+## Usage
+
+
+## <a name="references"></a> References
+
+- [The "Unicode Problem"](https://developer.mozilla.org/en-US/docs/Glossary/Base64#the_unicode_problem)
---
+++ app/index.js
@@ -0,0 +1,18 @@
+// Copyright (c) Rafa Calvo
+// SPDX-License-Identifier: MIT
+
+function base64ToBytes(base64) {
+    const binString = atob(base64);
+    return Uint8Array.from(binString, (m) => m.codePointAt(0));
+}
+
+function bytesToBase64(bytes) {
+    const binString = Array.from(bytes, (byte) =>
+        String.fromCodePoint(byte),
+    ).join("");
+    return btoa(binString);
+}
+
+// Usage
+bytesToBase64(new TextEncoder().encode("a Ā 𐀀 文 🦄")); // "YSDEgCDwkICAIOaWhyDwn6aE"
+new TextDecoder().decode(base64ToBytes("YSDEgCDwkICAIOaWhyDwn6aE")); // "a Ā 𐀀 文 🦄"
